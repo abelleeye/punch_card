eb78822f951087b7f142a5efb5600d24831c217e
abel <abel@abellee.cn>
2021-04-17 12:44:34 +0800

    init

---
+++ punch_card/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  punch_card
+//
+//  Created by Abel Lee on 2020/6/5.
+//  Copyright © 2020 Abel Lee. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
